58ecd4219f3013ac709ff4a3b49f1bc46605ef65
mm-sakshig <138591084+mm-sakshig@users.noreply.github.com>
2023-07-04 18:19:37 +0530

    Add files via upload

---
+++ Data/DbContextClass.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store_Procedure_Demo.Entities;
+
+namespace Store_Procedure_Demo.Data
+{
+    public class DbContextClass : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DbContextClass(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
